38a0e405b4543c384339a3697a39ea4528bef2b7
natuyade <natuyade@tuta.io>
2026-03-03 23:10:39 +0900

    fix issue playing ogg

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use leptos::prelude::*;
 fn setup() {
     App::new()
         .add_plugins(DefaultPlugins
-            .set(
+            /*.set(
                 WindowPlugin {
                     primary_window: Some(
                         Window {
@@ -15,7 +15,7 @@ fn setup() {
                     ),
                     ..default()
                 }
-            )
+            )*/
             .set(
                 AssetPlugin {
                     meta_check: bevy::asset::AssetMetaCheck::Never,
@@ -36,7 +36,7 @@ fn App() -> impl IntoView {
 }
 
 fn main() {
-    mount_to_body(App);
+    //mount_to_body(App);
     setup();
 }
 
@@ -55,17 +55,39 @@ struct PlayingBGM (bool);
 #[derive(Component)]
 struct BGM;
 
+#[derive(Component)]
+struct Cell;
+
+#[derive(Component)]
+struct StartButton;
+
 fn setup_title( mut commands: Commands, asset_server: Res<AssetServer> ) {
     commands.spawn(Camera2d);
 
     commands.insert_resource(GameAudio{
-        bgm: asset_server.load("sounds/bgm.wav"),
+        bgm: asset_server.load("sounds/bgm.ogg"),
         cell_click: asset_server.load("sounds/open_cell.wav"),
-        start: asset_server.load("sounds/start.wav"),
+        start: asset_server.load("sounds/start.mp3"),
         failed: asset_server.load("sounds/failed.wav"),
         setting: asset_server.load("sounds/setting_button.wav"),
     });
 
+    commands.spawn((
+        Node {
+            position_type: PositionType::Relative,
+            top: Val::Percent(50.),
+            left: Val::Percent(36.),
+            width: Val::Px(32.),
+            height: Val::Px(32.),
+            ..default()
+        },
+        BackgroundColor (Color::srgb(0.,0.,1.)),
+        Button,
+        PlayingBGM { 0: false },
+        children![
+            Text::new("ogg")
+        ]
+        ));
 
     commands.spawn((
         Node {
@@ -76,23 +98,30 @@ fn setup_title( mut commands: Commands, asset_server: Res<AssetServer> ) {
             height: Val::Px(32.),
             ..default()
         },
-        BackgroundColor (Color::srgb(1.,1.,1.)),
+        BackgroundColor (Color::srgb(1.,0.,1.)),
         Button,
-        ));
+        Cell,
+        children![
+            Text::new("wav")
+        ]
+    ));
 
     commands.spawn((
         Node {
             position_type: PositionType::Relative,
             top: Val::Percent(50.),
-            left: Val::Percent(32.),
+            left: Val::Percent(64.),
             width: Val::Px(32.),
             height: Val::Px(32.),
             ..default()
         },
-        PlayingBGM { 0: false },
+        BackgroundColor (Color::srgb(0.,1.,1.)),
         Button,
-        BackgroundColor (Color::srgb(0.,0.,1.)),
-        ));
+        StartButton,
+        children![
+            Text::new("mp3")
+        ]
+    ));
 }
 
 use bevy::ecs::query::With;
@@ -101,11 +130,12 @@ fn audio_button(
     audio: Res<GameAudio>,
     audio_assets: Res<Assets<AudioSource>>,
     mut commands: Commands,
-    mut query: Query<(&Interaction, &mut BackgroundColor),(Without<PlayingBGM>, Changed<Interaction>)>,
-    mut query2: Query<(&Interaction, &mut BackgroundColor, &mut PlayingBGM), (With<Button>, Changed<Interaction>)>,
+    mut bgm_query: Query<(&Interaction, &mut BackgroundColor, &mut PlayingBGM), (With<PlayingBGM>, Without<Cell>, Without<StartButton>, Changed<Interaction>)>,
+    mut cell_query: Query<(&Interaction, &mut BackgroundColor),(With<Cell>, Without<PlayingBGM>, Without<StartButton>, Changed<Interaction>)>,
+    mut start_query: Query<(&Interaction, &mut BackgroundColor),(With<StartButton>, Without<PlayingBGM>, Without<Cell>, Changed<Interaction>)>,
     bgmentity: Query<Entity, With<BGM>>
 ) {
-    for (ints, mut bgcolor, mut playing) in &mut query2 {
+    for (ints, mut bgcolor, mut playing) in &mut bgm_query {
 
             match *ints {
                 Interaction::Pressed => {
@@ -136,7 +166,7 @@ fn audio_button(
 
     }
 
-    for (ints, mut bgcolor) in &mut query {
+    for (ints, mut bgcolor) in &mut cell_query {
         match *ints {
             Interaction::Pressed => {
                 if audio_assets.get(&audio.cell_click).is_none() {
@@ -145,10 +175,27 @@ fn audio_button(
                 commands.spawn((AudioPlayer::new(audio.cell_click.clone()), PlaybackSettings::DESPAWN));
             }
             Interaction::Hovered => {
-                *bgcolor = BackgroundColor(Color::srgb(0., 1., 0.));
+                *bgcolor = BackgroundColor(Color::srgb(0., 1., 1.));
             }
             Interaction::None => {
-                *bgcolor = BackgroundColor(Color::srgb(1., 1., 1.));
+                *bgcolor = BackgroundColor(Color::srgb(1., 0., 1.));
+            }
+        }
+    }
+
+    for (ints, mut bgcolor) in &mut start_query {
+        match *ints {
+            Interaction::Pressed => {
+                if audio_assets.get(&audio.cell_click).is_none() {
+                    return;
+                }
+                commands.spawn((AudioPlayer::new(audio.start.clone()), PlaybackSettings::DESPAWN));
+            }
+            Interaction::Hovered => {
+                *bgcolor = BackgroundColor(Color::srgb(1., 1., 0.));
+            }
+            Interaction::None => {
+                *bgcolor = BackgroundColor(Color::srgb(0., 1., 1.));
             }
         }
     }
